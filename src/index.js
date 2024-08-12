3423e74cdf9aa01432ba12a7a487384d0cfc888d
VladTca <votsats@gmail.com>
2024-08-12 13:50:02 +0200

    ✨ Add .idea folder to .gitignore and refactor index.js function to check brackets 👀

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let bracketMap = new Map();
+    let openBrackets = new Set();
+    let closeBrackets = new Set();
+
+
+    for (let [open, close] of bracketsConfig) {
+        bracketMap.set(close, open);
+        openBrackets.add(open);
+        closeBrackets.add(close);
+    }
+
+    for (let char of str) {
+        if (openBrackets.has(char) && (!closeBrackets.has(char) || stack[stack.length - 1] !== char)) {
+
+            stack.push(char);
+        } else if (closeBrackets.has(char)) {
+            // If it's a closing bracket
+            if (stack.length === 0 || stack.pop() !== bracketMap.get(char)) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
